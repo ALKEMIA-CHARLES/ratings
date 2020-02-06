147ced905942c655c44cf3b19fc54dd146aabd3d
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2020-02-06 18:32:48 +0300

    added profile and project models

--- ratings/models.py
+++ ratings/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from pyuploadcare.dj.models import ImageField
 from PIL import Image
 from django.urls import reverse
+from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
@@ -12,9 +13,15 @@ class project(models.Model):
     image = models.ImageField(default="default.jpg", upload_to='pictures' )
     description = models.TextField(max_length=250)
     link =  models.URLField(max_length=250)
+    post_date = models.DateTimeField(auto_now_add=True)
 
 
-
+    @classmethod
+    def show_projects(cls):
+        return cls.objects.order("post_date")[::1]
+    @classmethod
+    def search_projects_by_title(cls,search):
+        return cls.objects.filter(name__icontains=search)
 class profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     image = models.ImageField(default="default.jpg", upload_to='pictures')
